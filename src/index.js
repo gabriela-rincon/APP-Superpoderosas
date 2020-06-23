eca91ace471c74e3d8f8d2c36934ecfb5807b2ef
Juliana Jimenez <nani.jimenez@hotmail.com>
2020-06-23 08:19:22 -0500

    Ultimos detalles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
-console.log(validator);
+const objectdata = document.getElementById("formulario1");
+ingresar = document.getElementById('ingresar');
+ingresar.addEventListener("click",pagar);
+const objectdata = document.getElementById("formulario1");
+objectdata.addEventListener("submit", enmascarar); 
+//objectdata.addEventListener("submit", validar);        //addEventListener
+
+    //var sumatoria=0;
+//var dato="fsdfsdf90";
+    //alert(validator.maskify(dato));
+//console.log(validator.maskify("datonumero"));
+
+
+function enmascarar(){
+    const datonumero = document.getElementById("number").value;
+    alert ("Su número de tarjeta es " + validator.maskify(datonumero) + "\n" + validator.isValid(datonumero))
+    
+}
+
+/*function validar(){
+    const datonumero = document.getElementById("number").value;
+    alert (validator.isValid(datonumero))
+
+}*/
+///Console.log(validator.maskify(datonumero)); //variable, funcion, metodo
+//console.log(validator.isValid(30630138407745)) ; 
